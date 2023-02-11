4c42007edbe8561ab6587231de1c7f52f77fe94b
Tadesse Jemal <tadessejemal63@gmail.com>
2023-02-11 19:16:27 +0300

    add resume in home page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,6 +3,7 @@ import "./About.css";
 import Header from "./../Header/Header.jsx"
 import aboutVector from "./../../assets/about_vector.png";
 import aboutAnime from "./../../assets/about_anime.gif";
+import Resume from '../../assets/Resume.pdf';
 function About() {
   return (
     <div className='section-container'>
@@ -14,12 +15,12 @@ function About() {
         <div className='about-main-left'>
             <h3 className='about-sub-heading'>Developer</h3>
             <p className='about-sub-heading-details'>
-              I’m a self-taught <u>full stack engineer</u>. building software for people and businesses. I love building software that solves real-world problems, add value to society through technology.
+              Hello I'm a Web developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don't hesitate to contact me.
             </p>
-            <h3 className='about-sub-heading'>Blogger</h3>
-            <p className='about-sub-heading-details'>
-              I've been writing blogs from around 3 years now, i used to write on Quora then i moved to hashnode now. which is specially dedicated for IT people. you can read my articles <a href="https://anandbaraik.hashnode.dev/" target="_blank" rel="noreferrer">here!</a>
-            </p>
+            <a href={Resume} download>
+                {' '}
+                <button className="btn highlighted-btn"><h3>Get Resume</h3></button>
+            </a>
         </div>
         <div className='about-main-right'>
             <img 
