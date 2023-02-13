53c36ce6d3f29f1a14463d7c4f69948f3d2a68a4
Tadesse Jemal <tadessejemal63@gmail.com>
2023-02-13 17:53:36 +0300

    Add skill data

---
+++ src/assets/skillsData.js
@@ -0,0 +1,38 @@
+export const skillList = [
+    {
+        skillName: 'HTML',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096301-84bb07bb-c807-46e4-a7d1-72e3f86c611c.png',
+    },
+    {
+        skillName: 'CSS',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096288-b6a601b7-7f5c-489e-be56-f9a7d61208f4.png',
+    },
+    {
+        skillName: 'JavaScript',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096309-ad1895af-57fc-4744-ad4c-35888c4f125b.png',
+    },
+    {
+        skillName: 'React',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096335-c6a18931-394d-444f-918a-51e1d052347b.png',
+    },
+    {
+        skillName: 'Redux',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096339-387cbc59-2630-458b-a2b7-a652245ea58a.png',
+    },
+    {
+        skillName: 'Ruby',
+        skillUrl: 'https://thumbs.dreamstime.com/b/ruby-round-over-white-background-d-illustration-74884059.jpg',
+    },
+    {
+        skillName: 'Ruby on Rails',
+        skillUrl: 'https://assets.stickpng.com/images/62c48bbdd884e8c372162223.png',
+    },
+    {
+        skillName : 'Mysql',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096321-7dc0e110-e99b-49c2-831b-aa72ede1a82e.png'
+    },
+    {
+        skillName : 'PostgresSQL',
+        skillUrl: 'https://icon-library.com/images/postgres-icon/postgres-icon-15.jpg'
+    },
+];
