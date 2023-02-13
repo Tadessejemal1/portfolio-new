6b44ce598baedc87178ab0202fd0d6811230fd18
Tadesse Jemal <tadessejemal63@gmail.com>
2023-02-13 17:09:58 +0300

    Add project in router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import {HashRouter, Route} from "react-router-dom";
 import NavBar from "./../NavBar/NavBar.jsx";
 import Home from "./../Home/Home.jsx";
 import About from "./../About/About.jsx";
+import Projects from "./../Projects/Projects.jsx";
 function App() {
   return (
     <div>
@@ -13,6 +14,7 @@ function App() {
           <NavBar/>
           <Route path="/" exact component={Home}/>
           <Route path="/about" exact component={About}/>
+          <Route path="/projects" exact component={Projects}/>
         </div>
       </HashRouter>
     </div>
