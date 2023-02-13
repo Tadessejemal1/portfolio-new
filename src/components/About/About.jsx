7832bf53f66e9dec409418eebe974d95a7fc6433
Tadesse Jemal <tadessejemal63@gmail.com>
2023-02-13 20:58:41 +0300

    Update about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./About.css";
 import Header from "./../Header/Header.jsx"
+import Footer from "./../Footer/Footer.jsx"
 import aboutVector from "./../../assets/about_vector.png";
 import aboutAnime from "./../../assets/about_anime.gif";
 import Resume from '../../assets/Resume.pdf';
+
 function About() {
   return (
     <div className='section-container'>
@@ -13,7 +15,7 @@ function About() {
       </Header>
       <div className='about-main'>
         <div className='about-main-left'>
-            <h3 className='about-sub-heading'>Developer</h3>
+            <h3 className='about-sub-heading'>Web Developer</h3>
             <p className='about-sub-heading-details'>
               Hello I'm a Web developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don't hesitate to contact me.
             </p>
@@ -31,6 +33,11 @@ function About() {
             />
         </div>
       </div>
+      <Footer 
+        phrase="Check out my "
+        link="projects!"
+        toAddress="/projects">
+      </Footer>
       <div className='vector-frame'>
         <img src={aboutVector}
           alt="about"
