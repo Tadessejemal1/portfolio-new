c5d3f290420203dee05458eb4c250e76ab1fb35b
Tadesse Jemal <tadessejemal63@gmail.com>
2023-02-13 17:07:37 +0300

    Add javascript function

---
+++ src/components/Projects/ProjectCard.jsx
@@ -0,0 +1,59 @@
+import "./Projects.css";
+function ProjectCard(
+    {projectName,
+    projectDescription,
+    imageUrl,
+    projectUrl,
+    skills,
+sourceUrl}) {
+
+    // let languages = skills.map((skill, i) => {
+    //     return (
+    //       <li key={i} value={skill}>
+    //         {skill}
+    //       </li>
+    //     ).join;
+    // });
+
+  return (
+    <div className="project-card">
+        <div className="image-container">
+            <a href={projectUrl}
+                className="project-external-link"
+                target="_blank" rel="noreferrer"
+                >
+                <img src={imageUrl} 
+                    alt={projectName}
+                    className="project-image"
+                    loading="lazy"
+                    />
+            </a>
+        </div>
+        <div className="project-details-container">
+            <h2 className="project-heading">
+                {projectName}
+            </h2>
+            <p className="project-details">
+                {projectDescription}
+            </p>
+            <ul className="languages-list">
+                {skills}
+            </ul>
+            <div className="project-stack">
+                <a href={projectUrl}
+                    className="project-live-link"
+                    target="_blank" rel="noreferrer">
+                    View live
+                </a>
+                <a href={sourceUrl}
+                    className="project-live-link"
+                    target="_blank" rel="noreferrer">
+                    Source Code
+                </a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProjectCard
