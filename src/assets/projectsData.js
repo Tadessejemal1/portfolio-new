af4040c6140b39a1f127b548f6cf9ea0da34962c
Tadesse Jemal <tadessejemal63@gmail.com>
2023-02-13 20:55:58 +0300

    update project data

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -5,7 +5,7 @@ export const projectsData = [
         projectDescription:
             'This project was created through the Microverse software development education course to amplify understanding of Ruby and Ruby on Rails by building a mobile web application where users can log in and monitor their expenses. It also provides an option to classify the outgoings into various classes',
         imageUrl: 'https://tadessejemal1.github.io/portfolio-personal/images/Capture8.JPG',
-        skills: 'Technologies:  Ruby-on-Rails , PostgresSQL',
+        skills: 'Built:  Ruby-on-Rails , PostgresSQL',
         projectUrl: 'https://budget-app-project.onrender.com/',
         sourceUrl: 'https://github.com/Tadessejemal1/Budget-App.git',
     },
@@ -15,7 +15,7 @@ export const projectsData = [
         projectDescription:
             'The JavaScript capstone project is about building our own web application based on an external API. we were select an API that provides data about a topic that you like and then build the webapp around it. The webapp have 2 user interfaces A home page showing a list of items that you can like. A popup window with more data about an item that you can use to comment on it or reserve it for a period of time.',
         imageUrl: 'https://tadessejemal1.github.io/portfolio-personal/images/Capture5.JPG',
-        skills: 'Technologies:  HTML, CSS, JavaScript',
+        skills: 'Built:  HTML, CSS, JavaScript',
         projectUrl: 'https://kumilachew-g.github.io/JavaScript-Capstone/dist/',
         sourceUrl: 'https://github.com/Tadessejemal1/JavaScript-Capstone.git',
 
@@ -25,9 +25,9 @@ export const projectsData = [
         projectName: 'Hotel Booking App',
         projectDescription: `Hotel Booking App is a Hotel Booking (SPA) where the users can register themselves, login and reserve a set of different Hotels. It is built and connected by using two different repos, including the server side( Our Backend) with Ruby on Rails, and the client side(Our Front-end) with React/Redux.''.`,
         imageUrl: 'https://tadessejemal1.github.io/portfolio-personal/images/Capture2.JPG',
-        skills: 'Technologies:  Ruby OnRails, PostgreSQL, React.JS',
-        projectUrl: 'https://euphonious-cactus-a77132.netlify.app/',
-        sourceUrl: 'https://github.com/Tadessejemal1/math-magicians.git',
+        skills: 'Built:  Ruby OnRails, PostgreSQL, React.JS',
+        projectUrl: 'https://inspiring-kashata-9d9dfc.netlify.app/',
+        sourceUrl: 'https://github.com/Tadessejemal1/hotel-booking-frontend.git',
     },
 
     {
@@ -35,7 +35,7 @@ export const projectsData = [
         projectDescription:
             'A web application for a company that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions. Using real live data from the SpaceX API.',
         imageUrl: 'https://tadessejemal1.github.io/portfolio-personal/images/Capture6.JPG',
-        skills: 'Technologies:  React.Js,   Redux,   JavaScript',
+        skills: 'Built:  React.Js,   Redux,   JavaScript',
         projectUrl: 'https://lokurasrlz.github.io/Space-Travelers/',
         sourceUrl: 'https://github.com/Tadessejemal1/Space-Travelers-Hub.git',
         
@@ -46,7 +46,7 @@ export const projectsData = [
         projectDescription:
             'Math magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to make simple calculations and read a random math-related quote.',
         imageUrl: 'https://tadessejemal1.github.io/portfolio-personal/images/Capture7.JPG',
-        skills: 'Technologies:  React.Js, React-Router-Dom',
+        skills: 'Built:  React.Js, React-Router-Dom',
         projectUrl: 'https://euphonious-cactus-a77132.netlify.app/',
         sourceUrl: 'https://github.com/Tadessejemal1/math-magicians.git',
     },
@@ -56,7 +56,7 @@ export const projectsData = [
         projectDescription:
             'In this website you will add scores and also see your scores and I am using a api for fetch the data from api and also add data to the api.',
         imageUrl: 'https://tadessejemal1.github.io/portfolio-personal/images/Capture3.JPG',
-        skills: 'Technologies:  HTML, CSS, JavaScript',
+        skills: 'Built:  HTML, CSS, JavaScript',
         projectUrl: 'https://tadessejemal1.github.io/Leaderboard-web/dist/',
         sourceUrl: 'https://github.com/Tadessejemal1/Leaderboard-web.git',
     },
